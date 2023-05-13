30f773f32bf553a34648aa946ff26da171cb9e9b
Mgen <m@mgenware.com>
2023-05-14 02:03:09 +0800

    Split jsonx methods into 'OrDefault` and 'OrNil` versions

--- jsonx/get_values.go
+++ jsonx/get_values.go
@@ -1,60 +1,131 @@
 package jsonx
 
-// GetString retrieves a string value for the given key in the dictionary, or a default value if it does not exist.
-func GetString(dict map[string]interface{}, key string) string {
+// GetStringOrDefault retrieves a string value for the given key in the dictionary, or a default value if it does not exist.
+func GetStringOrDefault(dict map[string]interface{}, key string) string {
 	val, _ := dict[key].(string)
 	return val
 }
 
-// GetFloat64 retrieves a float64 value for the given key in the dictionary, or a default value if it does not exist.
-func GetFloat64(dict map[string]interface{}, key string) float64 {
+// GetStringOrNil retrieves a string value for the given key in the dictionary, or nil if it does not exist.
+func GetStringOrNil(dict map[string]interface{}, key string) *string {
+	val, ok := dict[key].(string)
+	if ok {
+		return &val
+	}
+	return nil
+}
+
+// GetFloat64OrDefault retrieves a float64 value for the given key in the dictionary, or a default value if it does not exist.
+func GetFloat64OrDefault(dict map[string]interface{}, key string) float64 {
 	val, _ := dict[key].(float64)
 	return val
 }
 
-// GetInt retrieves an int value for the given key in the dictionary, or a default value if it does not exist.
-func GetInt(dict map[string]interface{}, key string) int {
+// GetFloat64OrNil retrieves a float64 value for the given key in the dictionary, or nil if it does not exist.
+func GetFloat64OrNil(dict map[string]interface{}, key string) *float64 {
+	val, ok := dict[key].(float64)
+	if ok {
+		return &val
+	}
+	return nil
+}
+
+// GetIntOrDefault retrieves an int value for the given key in the dictionary, or a default value if it does not exist.
+func GetIntOrDefault(dict map[string]interface{}, key string) int {
 	// All numbers parsed from JSON are float64
-	val := GetFloat64(dict, key)
+	val := GetFloat64OrDefault(dict, key)
 	return int(val)
 }
 
-// GetUint retrieves a uint value for the given key in the dictionary, or a default value if it does not exist.
-func GetUint(dict map[string]interface{}, key string) uint {
+// GetIntOrNil retrieves an int value for the given key in the dictionary, or nil if it does not exist.
+func GetIntOrNil(dict map[string]interface{}, key string) *int {
 	// All numbers parsed from JSON are float64
-	val := GetFloat64(dict, key)
+	val := GetFloat64OrNil(dict, key)
+	if val == nil {
+		return nil
+	}
+	intVal := int(*val)
+	return &intVal
+}
+
+// GetUintOrDefault retrieves a uint value for the given key in the dictionary, or a default value if it does not exist.
+func GetUintOrDefault(dict map[string]interface{}, key string) uint {
+	// All numbers parsed from JSON are float64
+	val := GetFloat64OrDefault(dict, key)
 	return uint(val)
 }
 
-// GetInt64 retrieves an int64 value for the given key in the dictionary, or a default value if it does not exist.
-func GetInt64(dict map[string]interface{}, key string) int64 {
+// GetUintOrNil retrieves a uint value for the given key in the dictionary, or nil if it does not exist.
+func GetUintOrNil(dict map[string]interface{}, key string) *uint {
 	// All numbers parsed from JSON are float64
-	val := GetFloat64(dict, key)
+	val := GetFloat64OrNil(dict, key)
+	if val == nil {
+		return nil
+	}
+	uintVal := uint(*val)
+	return &uintVal
+}
+
+// GetInt64OrDefault retrieves an int64 value for the given key in the dictionary, or a default value if it does not exist.
+func GetInt64OrDefault(dict map[string]interface{}, key string) int64 {
+	// All numbers parsed from JSON are float64
+	val := GetFloat64OrDefault(dict, key)
 	return int64(val)
 }
 
-// GetUint64 retrieves a uint64 value for the given key in the dictionary, or a default value if it does not exist.
-func GetUint64(dict map[string]interface{}, key string) uint64 {
+// GetInt64OrNil retrieves an int64 value for the given key in the dictionary, or a default value if it does not exist.
+func GetInt64OrNil(dict map[string]interface{}, key string) *int64 {
 	// All numbers parsed from JSON are float64
-	val := GetFloat64(dict, key)
+	val := GetFloat64OrNil(dict, key)
+	if val == nil {
+		return nil
+	}
+	int64Val := int64(*val)
+	return &int64Val
+}
+
+// GetUint64OrDefault retrieves a uint64 value for the given key in the dictionary, or a default value if it does not exist.
+func GetUint64OrDefault(dict map[string]interface{}, key string) uint64 {
+	// All numbers parsed from JSON are float64
+	val := GetFloat64OrDefault(dict, key)
 	return uint64(val)
 }
 
-// GetBool retrieves a bool value for the given key in the dictionary, or a default value if it does not exist.
-func GetBool(dict map[string]interface{}, key string) bool {
+// GetUint64 retrieves a uint64 value for the given key in the dictionary, or nil if it does not exist.
+func GetUint64OrNil(dict map[string]interface{}, key string) *uint64 {
+	// All numbers parsed from JSON are float64
+	val := GetFloat64OrNil(dict, key)
+	if val == nil {
+		return nil
+	}
+	uint64Val := uint64(*val)
+	return &uint64Val
+}
+
+// GetBoolOrDefault retrieves a bool value for the given key in the dictionary, or a default value if it does not exist.
+func GetBoolOrDefault(dict map[string]interface{}, key string) bool {
 	val, _ := dict[key].(bool)
 	return val
 }
 
-// GetDict retrieves a dictionary value for the given key in the dictionary, or nil if it does not exist.
-func GetDict(dict map[string]interface{}, key string) map[string]interface{} {
+// GetBoolOrNil retrieves a bool value for the given key in the dictionary, or a default value if it does not exist.
+func GetBoolOrNil(dict map[string]interface{}, key string) *bool {
+	val, ok := dict[key].(bool)
+	if ok {
+		return &val
+	}
+	return nil
+}
+
+// GetDictOrNil retrieves a dictionary value for the given key in the dictionary, or nil if it does not exist.
+func GetDictOrNil(dict map[string]interface{}, key string) map[string]interface{} {
 	val, _ := dict[key].(map[string]interface{})
 	return val
 }
 
 // GetDictOrEmpty retrieves a dictionary value for the given key in the dictionary, or an empty dictionary if it does not exist.
 func GetDictOrEmpty(dict map[string]interface{}, key string) map[string]interface{} {
-	val := GetDict(dict, key)
+	val := GetDictOrNil(dict, key)
 	if val == nil {
 		return make(map[string]interface{})
 	}
@@ -62,14 +133,14 @@ func GetDictOrEmpty(dict map[string]interface{}, key string) map[string]interfac
 }
 
 // GetArray retrieves an array value for the given key in the dictionary, or nil if it does not exist.
-func GetArray(dict map[string]interface{}, key string) []interface{} {
+func GetArrayOrNil(dict map[string]interface{}, key string) []interface{} {
 	val, _ := dict[key].([]interface{})
 	return val
 }
 
 // GetArrayOrEmpty retrieves the value for the given key in the dictionary, or an empty dictionary if it does not exist.
 func GetArrayOrEmpty(dict map[string]interface{}, key string) []interface{} {
-	val := GetArray(dict, key)
+	val := GetArrayOrNil(dict, key)
 	if val == nil {
 		return make([]interface{}, 0)
 	}
--- jsonx/get_values_test.go
+++ jsonx/get_values_test.go
@@ -16,44 +16,74 @@ func init() {
 	sampleDict = dict
 }
 
-func TestGetString(t *testing.T) {
-	test.Assert(t, GetString(sampleDict, "s"), "haha")
-	test.Assert(t, GetString(sampleDict, "_"), "")
+func TestGetStringOrDefault(t *testing.T) {
+	test.Assert(t, GetStringOrDefault(sampleDict, "s"), "haha")
+	test.Assert(t, GetStringOrDefault(sampleDict, "_"), "")
 }
 
-func TestGetBool(t *testing.T) {
-	test.Assert(t, GetBool(sampleDict, "b"), true)
-	test.Assert(t, GetBool(sampleDict, "_"), false)
+func TestGetStringOrNil(t *testing.T) {
+	test.Assert(t, *GetStringOrNil(sampleDict, "s"), "haha")
+	test.Assert(t, GetStringOrNil(sampleDict, "_"), (*string)(nil))
 }
 
-func TestGetInt(t *testing.T) {
-	test.Assert(t, GetInt(sampleDict, "nn"), -1)
-	test.Assert(t, GetInt(sampleDict, "_"), 0)
+func TestGetBoolOrDefault(t *testing.T) {
+	test.Assert(t, GetBoolOrDefault(sampleDict, "b"), true)
+	test.Assert(t, GetBoolOrDefault(sampleDict, "_"), false)
 }
 
-func TestGetUint(t *testing.T) {
-	test.Assert(t, GetUint(sampleDict, "pn"), uint(123))
-	test.Assert(t, GetUint(sampleDict, "_"), uint(0))
+func TestGetBoolOrNil(t *testing.T) {
+	test.Assert(t, *GetBoolOrNil(sampleDict, "b"), true)
+	test.Assert(t, GetBoolOrNil(sampleDict, "_"), (*bool)(nil))
 }
 
-func TestGetInt64(t *testing.T) {
-	test.Assert(t, GetInt64(sampleDict, "nn"), int64(-1))
-	test.Assert(t, GetInt64(sampleDict, "_"), int64(0))
+func TestGetIntOrDefault(t *testing.T) {
+	test.Assert(t, GetIntOrDefault(sampleDict, "nn"), -1)
+	test.Assert(t, GetIntOrDefault(sampleDict, "_"), 0)
 }
 
-func TestGetUint64(t *testing.T) {
-	test.Assert(t, GetUint64(sampleDict, "pn"), uint64(123))
-	test.Assert(t, GetUint64(sampleDict, "_"), uint64(0))
+func TestGetIntOrNil(t *testing.T) {
+	test.Assert(t, *GetIntOrNil(sampleDict, "nn"), -1)
+	test.Assert(t, GetIntOrNil(sampleDict, "_"), (*int)(nil))
 }
 
-func TestGetDict(t *testing.T) {
-	d1 := GetDict(sampleDict, "dict")
+func TestGetUintOrDefault(t *testing.T) {
+	test.Assert(t, GetUintOrDefault(sampleDict, "pn"), uint(123))
+	test.Assert(t, GetUintOrDefault(sampleDict, "_"), uint(0))
+}
+
+func TestGetUintOrNil(t *testing.T) {
+	test.Assert(t, *GetUintOrNil(sampleDict, "pn"), uint(123))
+	test.Assert(t, GetUintOrNil(sampleDict, "_"), (*uint)(nil))
+}
+
+func TestGetInt64OrDefault(t *testing.T) {
+	test.Assert(t, GetInt64OrDefault(sampleDict, "nn"), int64(-1))
+	test.Assert(t, GetInt64OrDefault(sampleDict, "_"), int64(0))
+}
+
+func TestGetInt64OrNil(t *testing.T) {
+	test.Assert(t, *GetInt64OrNil(sampleDict, "nn"), int64(-1))
+	test.Assert(t, GetInt64OrNil(sampleDict, "_"), (*int64)(nil))
+}
+
+func TestGetUint64OrDefault(t *testing.T) {
+	test.Assert(t, GetUint64OrDefault(sampleDict, "pn"), uint64(123))
+	test.Assert(t, GetUint64OrDefault(sampleDict, "_"), uint64(0))
+}
+
+func TestGetUint64OrNil(t *testing.T) {
+	test.Assert(t, *GetUint64OrNil(sampleDict, "pn"), uint64(123))
+	test.Assert(t, GetUint64OrNil(sampleDict, "_"), (*uint64)(nil))
+}
+
+func TestGetDictOrNil(t *testing.T) {
+	d1 := GetDictOrNil(sampleDict, "dict")
 	d2 := make(map[string]interface{})
 	d2["a"] = true
 	d2["b"] = []interface{}{"element1", "element2"}
 	test.Assert(t, d1, d2)
 
-	test.Assert(t, GetDict(sampleDict, "arr"), map[string]interface{}(nil))
+	test.Assert(t, GetDictOrNil(sampleDict, "arr"), map[string]interface{}(nil))
 }
 
 func TestGetDictOrEmpty(t *testing.T) {
@@ -66,12 +96,12 @@ func TestGetDictOrEmpty(t *testing.T) {
 	test.Assert(t, GetDictOrEmpty(sampleDict, "arr"), make(map[string]interface{}))
 }
 
-func TestGetArray(t *testing.T) {
-	d1 := GetArray(sampleDict, "arr")
+func TestGetArrayOrNil(t *testing.T) {
+	d1 := GetArrayOrNil(sampleDict, "arr")
 	d2 := []interface{}{float64(1), float64(2), "haha"}
 	test.Assert(t, d1, d2)
 
-	test.Assert(t, GetArray(sampleDict, "dict"), []interface{}(nil))
+	test.Assert(t, GetArrayOrNil(sampleDict, "dict"), []interface{}(nil))
 }
 
 func TestGetArrayOrEmpty(t *testing.T) {
